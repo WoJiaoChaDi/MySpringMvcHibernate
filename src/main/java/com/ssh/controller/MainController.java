5313322525d2c90167021c335d8919905b473a63
WoJiaoChaDi <32976595+WoJiaoChaDi@users.noreply.github.com>
2018-09-18 17:43:41 +0800

    springmvc初始版本
    springmvc初始版本

---
+++ src/main/java/com/ssh/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.ssh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test(){
+//        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
+        return "test";
+    }
+    
+    @RequestMapping(value = "index", method = RequestMethod.GET)
+    public String index(){
+//        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
+    	return "index";
+    }
+}
