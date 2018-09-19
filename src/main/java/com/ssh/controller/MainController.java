8c3b2d1369b7ac3d72c0ce3e642b5abc24a4bb8b
XuDong <264835229@qq.com>
2018-09-19 14:54:16 +0800

    springmvc+spring整合

--- src/main/java/com/ssh/controller/MainController.java
+++ src/main/java/com/ssh/controller/MainController.java
@@ -1,11 +1,17 @@
 package com.ssh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.ssh.service.TestService;
+
 @Controller
 public class MainController {
+	
+	@Autowired
+    private TestService testService;
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String test(){
@@ -17,5 +23,11 @@ public class MainController {
     public String index(){
 //        实际返回的是views/test.jsp ,spring-mvc.xml中配置过前后缀
     	return "index";
+    }
+    
+    @RequestMapping(value = "springtest", method = RequestMethod.GET)
+    public String springTest(){
+    	System.out.println(testService.test());
+        return "test";
     }
 }
