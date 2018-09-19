a89cb36b2f974c6f4cbe9f0ddddd23876da0dff8
XuDong <264835229@qq.com>
2018-09-19 15:17:57 +0800

    Spring+SpringMVC+hibernate整合

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MySpringMvcHibernate
 MySpringMvcHibernate
+
+参考文章：
+https://www.cnblogs.com/xiaowenbo/p/6980423.html
--- src/main/java/com/ssh/service/impl/TestServiceImpl.java
+++ src/main/java/com/ssh/service/impl/TestServiceImpl.java
@@ -3,10 +3,6 @@ package com.ssh.service.impl;
 import com.ssh.service.TestService;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by XRog
- * On 2/1/2017.12:58 AM
- */
 @Service
 public class TestServiceImpl implements TestService {
     public String test() {
